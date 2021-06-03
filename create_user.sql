1a746e9b030db7c65253c47385994760ef99dd50
srikanth4075 <46095027+srikanth4075@users.noreply.github.com>
2021-06-03 14:22:24 -0700

    Update create_user.sql

--- create_user.sql
+++ create_user.sql
@@ -1,1 +1,15 @@
 
+select
+	'CREATE USER "' || LOWER(TRIM(usename))|| '" with PASSWORD ' || CHR(39)|| 'MyP@ss1@#$' || CHR(39)||
+	case
+		when usecreatedb = 't' then ' CREATEDB'
+		else ' NOCREATEDB'
+	end ||
+	case
+		when usesuper = 't' then ' CREATEUSER'
+		else ' NOCREATEUSER'
+	end ||' connection limit '||trim(useconnlimit) ||';'
+from
+pg_user_info
+where
+usesysid >= 100 ;
